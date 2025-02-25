61272ab3e20d69e9774349ba7ee1a1ffcd39adf7
ibrahimsha23 <ibrahim.kasim@decathlon.com>
2025-02-25 08:20:49 +0530

    optimized approach - inverted implementatiomn

--- system_Design/inverted_index/implementation.py
+++ system_Design/inverted_index/implementation.py
@@ -1,20 +1,16 @@
+from collections import defaultdict
+
 data_warehouse = {
     'Doc1': 'apple banana orange',
     'Doc2': 'banana apple apple',
     'Doc3': 'orange banana',
 }
-inverted_index = {
-}
+
+inverted_index = defaultdict(list)
 
 for doc_id, doc_data in data_warehouse.items():
-    print(doc_id, doc_data)
-    tokens = doc_data.split()
-    for ele in set(tokens):
-        ele = ele.strip()
-        print(ele)
-        if ele in inverted_index:
-            inverted_index[ele].append(doc_id)
-        else:
-            inverted_index[ele] = [doc_id]
+    tokens = set(doc_data.split())
+    for ele in tokens:
+        inverted_index[ele].append(doc_id)
 
 print(inverted_index)
